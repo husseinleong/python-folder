433fbc9e8b582c308ec429b6cb8b8e76bcd0f63e
Your Name <marklevant20@gmail.com>
2024-11-10 21:13:52 +0100

    From Pycharm

--- Python General Projects/temperature.py
+++ Python General Projects/temperature.py
@@ -7,7 +7,9 @@ if unit == "C":
     temp = round((9 * temp) / 5 + 32, 1)
     print(f"The temperature in Fahrenheit is {temp} °F")
 elif unit == "F":
-    pass
+    temp = round((temp - 32) * 5 / 9 , 1)
+    print(f"The temperature in Celcius is: {temp}°C")
+
 
 else:
     print(f"{unit} is not a valid unit of measurement")
